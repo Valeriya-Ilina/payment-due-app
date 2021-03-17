8c7d9e449de8053d853729f520c24047478c221f
Valeriya Ilina <59905206+Valeriya-Ilina@users.noreply.github.com>
2021-03-17 17:43:31 -0400

    new bill page is created

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const app = express();
-const bills = require('./models/seed.js') // connected all bills
+const bills = require('./models/seed.js'); // connected all bills
+
+//allows for use of PUT and DELETE requests on our forms
+const methodOverride = require("method-override");
+app.use(methodOverride('_method'));
+
+//interpreting incoming request as JSON
+app.use(express.json());
+//allows to preceive the incoming object as a string or array
+app.use(express.urlencoded({extended: true}));
 
 //index route
 app.get('/bills', (req, res) => {
   res.render('index.ejs', {
     allBills: bills
+  });
+});
+
+//new route
+app.get('/bills/new', (req, res) => {
+  res.render('new.ejs')
+})
+//create route
+app.post('/bills', (req, res) => {
+  bills.push(req.body)
+  res.redirect('/bills')
+})
+
+//show route
+app.get('/bills/:index', (req, res) => {
+  res.render('show', {
+    bill: bills[req.params.index]
   });
 });
 
