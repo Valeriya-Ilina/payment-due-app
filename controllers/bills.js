4c6ef9cce1b28adcdb75511250206201c2d21999
Valeriya Ilina <59905206+Valeriya-Ilina@users.noreply.github.com>
2021-03-18 21:42:06 -0400

    connect to mongodb and create controllers

--- controllers/bills.js
+++ controllers/bills.js
@@ -6,6 +6,106 @@ const Bill = require('../models/bills.js');
 
 //CONTROLLERS
 
+//seed data to the database for testing purposes
+router.get('/seed', (req, res) => {
+  Bill.create(seed, (err, data) => {
+    res.redirect('/bills')
+  });
+});
 
+//index route
+router.get('/', (req, res) => {
+  //res.render('index.ejs', {
+    //allBills: bills
+  //});
+  Bill.find( {}, (err, foundBills) => {
+    if(err) {
+      console.log(err)
+      next(err)
+    } else {
+      res.render('index.ejs', {
+        allBills: foundBills
+      });
+    };
+  });
+});
+
+//new route
+router.get('/new', (req, res) => {
+  res.render('new.ejs')
+});
+//create route using post method
+router.post('/', (req, res) => {
+  //bills.push(req.body)
+  // add new item to Bills
+  if(req.body.autopay === 'on') {
+    req.body.autopay = true
+  } else {
+    req.body.autopay = false
+  }
+    //console.log(req.body)
+  Bill.create(req.body, (err, createdBill) => {
+    if (err) {
+      console.log(err)
+      res.send(err)
+    } else {
+      console.log(createdBill)
+      res.redirect('/bills')
+    };
+  });
+});
+
+//show route
+router.get('/:index', (req, res) => {
+  Bill.findById(req.params.index, (err, foundBill) => {
+    console.log(foundBill)
+    res.render('show.ejs', {
+      singleBill: foundBill,
+      index: req.params.index
+    });
+  });
+});
+
+//delete route
+router.delete('/:index', (req, res) => {
+  Bill.findByIdAndRemove(req.params.index, (err, data) => {
+    if(err) {
+      console.log(err)
+    } else {
+      //bills.splice(req.params.index, 1)//remove an element from the array
+      res.redirect('/bills')
+    };
+  });
+});
+
+//edit route
+router.get('/:index/edit', (req,res) => {
+  Bill.findById(req.params.index, (err, foundBill) => {
+    res.render('edit.ejs', {
+      singleBill: foundBill,
+      index: req.params.index
+    });
+  });
+});
+//put route
+//when we do edit, we change autopay value
+router.put('/:index', (req, res) => {
+  if(req.body.autopay === 'on'){
+    req.body.autopay = true
+  } else {
+    req.body.autopay = false
+  }
+  //console.log(req.body)
+  Bill.findByIdAndUpdate(req.params.index, req.body, { new: true }, (err, updatedBill) => {
+    // submit the form and update the whole product
+    updatedBill.name = req.body.name
+    updatedBill.billAmount = req.body.billAmount
+    updatedBill.dueDate = req.body.dueDate
+    updatedBill.autopay = req.body.autopay
+    updatedBill.payMethod = req.body.payMethod
+    updatedBill.notes = req.body.notes
+    res.redirect(`/bills/${req.params.index}`)
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const bills = require('./models/seed.js'); // connected all bills
 
+//requiring bills.js controller
+const billControllers = require('./controllers/bills');
+
 //allows for use of PUT and DELETE requests on our forms
 const methodOverride = require("method-override");
 app.use(methodOverride('_method'));
@@ -36,57 +39,9 @@ app.use(express.json());
 //allows to preceive the incoming object as a string or array
 app.use(express.urlencoded({extended: true}));
 
-//index route
-app.get('/bills', (req, res) => {
-  res.render('index.ejs', {
-    allBills: bills
-  });
-});
 
-//new route
-app.get('/bills/new', (req, res) => {
-  res.render('new.ejs')
-});
-//create route
-app.post('/bills', (req, res) => {
-  bills.push(req.body)
-  res.redirect('/bills')
-});
-
-//show route
-app.get('/bills/:index', (req, res) => {
-  res.render('show.ejs', {
-    singleBill: bills[req.params.index],
-    index: req.params.index
-  });
-});
-
-//delete route
-app.delete('/bills/:index', (req, res) => {
-  bills.splice(req.params.index, 1)//remove an element from the array
-  res.redirect('/bills')
-});
-
-//edit route
-app.get('/bills/:index/edit', (req,res) => {
-  res.render('edit.ejs', {
-    singleBill: bills[req.params.index],
-    index: req.params.index
-  });
-});
-//put route
-app.put('/bills/:index', (req, res) => {
-  if(req.body.autopay === 'on'){
-    req.body.autopay = true
-  } else {
-    req.body.autopay = false
-  }
-  console.log(req.body)
-  bills[req.params.index] = req.body
-  res.redirect(`/bills/${req.params.index}`)
-});
-
-
+//CONTROLLERS
+app.use('/bills', billControllers)
 
 
 app.listen(3000, () => {
